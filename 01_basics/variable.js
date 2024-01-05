b33b6684a63ba7675a14349423ad54d4a08004f8
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-05 12:52:33 +0000

    commited

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 12234;
+let name = "AnshYadav";
+var branch = "Artificial Intelligence";
+//const cannot be re defined 
+/*
+var is not ussed commenly , due to issue in block scope
+*/
+console.table([accountId,name,branch]);
