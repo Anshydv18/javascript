6fb63182017d9fdada3518d22e329ab098135fd6
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-07 09:41:13 +0000

    commited

---
+++ 01_basics/03datatypes.js
@@ -0,0 +1,15 @@
+/*
+datatype are
+1=>numbers =>2 to power 52
+bigint =>
+string=>
+boolean= true/false
+null
+undefined
+symbols
+*/
+
+let ans = "Ansh";
+console.log(typeof(ans));
+console.log(typeof(null)); //object , this is an object
+console.log(typeof(undefined));
