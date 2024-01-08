3b240d47da59f626d3e946756de8bc232cdc337e
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-08 07:34:35 +0000

    dataype summary conclude

---
+++ 01_basics/summay-dataype.js
@@ -0,0 +1,20 @@
+/*
+mainly the datatypes are of two types 
+1.primitive dataypes
+2.reference(non-primitive)
+
+=>primitive 7 types : string,boolean,numbers,bigInt ,null,undefined,symbols
+
+=>refernces Arrays,objects,functions
+
+*/
+
+const accoundId = Symbol('123');
+const reaccound = Symbol('123');
+console.log(accoundId===reaccound);
+
+let arr =["ansh","RamJanamYadav","MaltiDevi"];
+console.log(arr);
+
+//the typeof null is object and all the other dataypes of every datatype are same
+// the typeof references are the object functions of all 
