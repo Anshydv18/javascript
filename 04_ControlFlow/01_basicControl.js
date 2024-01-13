2cfc84b65885f283ed48676026516b505ae7a66f
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-13 05:04:02 +0000

    commited

---
+++ 04_ControlFlow/01_basicControl.js
@@ -0,0 +1,26 @@
+const  a= 6;
+//if(a<=5) console.log("it is greater than two"), console.log(a);
+// we can use scope like this , it will run be , but not a readable code , It's just a fancy code
+
+/*if(a<5){
+    console.log("number is less than five");
+}
+else if(a==5){
+    console.log("number is equal to 5");
+}
+else{
+    console.log("number is greater than 5");
+}
+*/
+
+//+++ we can use nested if also 
+
+/*if(a<7){
+    if(a>5){
+        console.log("the number is greater than 5 and less than 7");
+    }
+    else {
+        console.log("number is less than 5 and 7 both");
+    }
+}*/
+
---
+++ 04_ControlFlow/02_switch.js
@@ -0,0 +1,21 @@
+const month = "dec";
+
+switch(month){
+    case "jan": console.log('its january');
+    break;
+    case "feb": console.log('its february');
+    break;
+    case "march":console.log('its march');
+    break;
+    case "april":console.log('its April');
+    break;
+    case "may":console.log('its May');
+    break;
+    case "june":console.log('its june');
+    break;
+    case "july":console.log('its july');
+    break;
+    case "august":console.log('its august');
+    break;
+    default: console.log('not mentioned');
+}
