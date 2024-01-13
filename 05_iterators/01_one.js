d1b9ac06eca8858474d287fd8ad013f37b54fea5
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-13 05:46:29 +0000

    loops

---
+++ 05_iterators/01_one.js
@@ -0,0 +1,13 @@
+for(let i=0;i<=10;i++){
+    console.log(i);
+}
+
+//break and continue are also a feature of the loop in javascript
+
+while (condition) {
+    //statement
+}
+
+do {
+    //statement
+} while (condition);
